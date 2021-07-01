736e5ae7e262ec4bc353a5d09fc753adb95dd4c3
jennifertimbol <jennifertmbl@gmail.com>
2021-07-01 14:06:48 -0700

    form events

---
+++ events_eventobjs/app.js
@@ -0,0 +1,20 @@
+// document.querySelector('button').addEventListener('click', function () {
+//     alert("CLICK")
+// })
+
+document.querySelector('button').addEventListener('click', function (evt) {
+    console.log(evt)
+})
+
+const input = document.querySelector('input');
+input.addEventListener('keydown', function () {
+    console.log("KEYDOWN")
+})
+input.addEventListener('keyup', function () {
+    console.log("KEYUP")
+})
+
+input.addEventListener('keyup', function (e) {
+    console.log(e.key)
+    console.log(e.code)
+})
